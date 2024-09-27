5308f126d7944d1615ac5581ab60dfdb728a018d
marta <marta@martas-MacBook-Air.local>
2024-09-27 13:48:35 +0300

    Fix Flake8 ошибкиit message here

--- homework/user0/Homework_3/task#2.py
+++ homework/user0/Homework_3/task#2.py
@@ -1,8 +1,8 @@
 # задание: Даны числа x и y, получите x - y / 1 + xy
 
-x = float (input ("Введите число x = "))
-y = float (input ("Введите число y = "))
+x = float(input ("Введите число x = "))
+y = float(input ("Введите число y = "))
 
 expression = x - y / (1 + x * y)
 
-print (f"Результат: {expression}")
+print(f"Результат: {expression}")
