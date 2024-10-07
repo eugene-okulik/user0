480e748addebc48364d49b7547e3b8c0421823e9
marta <marta@martas-MacBook-Air.local>
2024-10-07 14:27:22 +0300

    Выполнено задание 3

---
+++ homework/user0/Homework_3/task_3.2.py
@@ -0,0 +1,10 @@
+'''
+Задание: Даны числа x и y, получите x - y / 1 + xy
+'''
+
+x = float(input ("Введите число x = "))
+y = float(input ("Введите число y = "))
+
+expression = x - y / (1 + x * y)
+
+print(f"Результат: {expression}")
