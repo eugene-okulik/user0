02eb6a2290ebba4128ab4720a3d204a2dace90c3
rsoc <135045867+rsoc-dev@users.noreply.github.com>
2024-10-15 13:09:22 +0200

    Update task_1.py

--- homework/user0/Homework_6/task_1.py
+++ homework/user0/Homework_6/task_1.py
@@ -1,7 +1,7 @@
 '''
 Задание №1
 Напишите программу, которая добавляет ‘ing’ к словам (к каждому слову)
-в тексте “Etiam tincidunt neque erat, quis molestie enim imperdiet vel. 
+в тексте “Etiam tincidunt neque erat, quis molestie enim imperdiet vel.
 Integer urna nisl, facilisis vitae semper at, dignissim vitae libero”
 и после этого выводит получившийся текст на экран. Знаки препинания
 не должны оказаться внутри слова. Если после слова идет запятая или точка,
@@ -13,11 +13,11 @@ text = (
     'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
 )
 
-words = text.split() # Разделяем text на слова
+words = text.split()  # Разделяем text на слова
 
-for word in words: # для слова ис списка слов
-    if word.endswith(',') or word.endswith('.'): # если слово заканчивается (включает символ) на , или . то
-        new_word = word[:-1] + 'ing' + word[-1] # для слова убираем последний символ + окончание, после чего + последний символ
+for word in words:  # для слова ис списка слов
+    if word.endswith(',') or word.endswith('.'):  # если слово заканчивается (включает символ) на , или . то
+        new_word = word[:-1] + 'ing' + word[-1]  # для слова убираем последний символ + окончание, после чего + последний символ
     else:
         new_word = word + 'ing'
     print(new_word, end=' ')
